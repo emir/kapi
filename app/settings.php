640176df88c989a39b97a324557d3dd3230d42cd
Fatih Kadir Akın <fka@fatihak.in>
2013-07-07 20:13:56 +0300

    initial commit

--- app/settings.php
+++ app/settings.php
@@ -2,7 +2,7 @@
 namespace KAPI;
 
 class Settings {
-  const DB_CONNECTION_STRING = "mysql:host=remote-mysql3.servage.net;dbname=hediyetavsiye";
+  const DB_CONNECTION_STRING = "mysql:host=;dbname=";
   const DB_USERNAME          = "";
   const DB_PASSWORD          = "";
 }
